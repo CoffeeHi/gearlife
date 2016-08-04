6647e1ec355d1f26c446b8a342b9d17090bc7d77
ChenXiang <codercx@sina.com>
2016-08-05 00:37:14 +0800

    解决了banner图片没缩放的bug，又多了banner切换闪屏的bug

--- js/index.js
+++ js/index.js
@@ -25,11 +25,44 @@ angular.module('index',[])
                 console.log('span_success ' + $scope.extend)
 
                 $scope.extend = angular.element('.banner ul li').index(angular.element(event.target).parent().parent()) + 1;
-
+               /* angular.forEach(angular.element('.banner ul li img'), function (item) {
+                    //console.log(item.src)
+                    var jqItem = angular.element(item);
+                    if(jqItem.next() === angular.element(event.target).parent()){
+                        jqItem.css('width','100%');
+                    }else{
+                        jqItem.removeAttr('width');
+                    }
+                });*/
+                angular.forEach(angular.element('.banner ul li img'), function (item) {
+                    //console.log(item.src)
+                    //console.log(angular.element(event.target).parent().prev()[0].currentSrc)
+                    if(item.src === angular.element(event.target).parent().prev()[0].currentSrc){
+                        angular.element(item).css('width','100%');
+                    }else{
+                        angular.element(item).css('width','');
+                    }
+                });
                 console.log('span_success ' + $scope.extend)
             }else if(event.target.nodeName === 'A'){
                 console.log('a_success')
                 $scope.extend = angular.element('.banner ul li').index(angular.element(event.target).parent()) + 1;
+                /*angular.forEach(angular.element('.banner ul li img'), function (item) {
+                    var jqItem = angular.element(item);
+                    //console.log(item.src)
+                    if(jqItem.next() === angular.element(event.target)){
+                        jqItem.css('width','100%');
+                    }else{
+                        jqItem.removeAttr('width');
+                    }
+                });*/
+                angular.forEach(angular.element('.banner ul li img'), function (item) {
+                    if(item.src === angular.element(event.target).prev()[0].currentSrc){
+                        angular.element(item).css('width','100%');
+                    }else{
+                        angular.element(item).css('width','');
+                    }
+                });
             }
         }
     }])
